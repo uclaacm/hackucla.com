800cb67ab432cbb7bc32d49f94284c32ea5747a9
shannonphu <shannonphu@sbcglobal.net>
2018-04-14 02:06:43 -0700

    refactor session with builder pattern constructor

--- api/models/schemas/session.js
+++ api/models/schemas/session.js
@@ -1,35 +1,124 @@
 let mongoose = require('mongoose');
 
-let Session = new mongoose.Schema({
-	number: { type: Number, required: true, unique: true },
-	secret: { type: String, required: true, unique: true },
-	name: { type: String, required: true },
-	desc: { type: String, required: true },
-	image: { type: String, required: true },
-	date: {
-		start: { type: Date, required: true },
-		end: { type: Date, required: true },
-	},
-	blogPostLink: { type: String },
-	project: {
-		points: { type: Number },
-		videoLink: { type: String },
-		slidesLink: { type: String },
-		submissionLink: { type: String },
-		sourceCodeLink: { type: String }
-	}
+let sessionSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    date: {
+        start: { type: Date, required: true },
+        end: { type: Date, required: true },
+    },
+    resources: {
+        sourceCodeLink: { type: String },
+        slidesLink: { type: String },
+        videoLink: { type: String },
+    },
+    project: {
+        submissionLink: { type: String }
+    }
 });
 
-Session.statics.getAll = function() {
-	return this.find({}).exec();
+// Session Schema Methods
+
+/**
+ * Creates an uninitialized Session object (not saved to DB yet).
+ * Utilizes builder pattern.
+ * @returns {Session} empty Session object
+ * @example
+ * Session.init()
+ */
+sessionSchema.statics.init = function () {
+    return new this();
+}
+
+/**
+ * Sets name of session
+ * @param {string} name
+ * @returns {Session} this session
+ * @example
+ * Session.init()
+ *        .withName("name")
+ *        .build()
+ *        .then(session => console.log(session));
+ */
+sessionSchema.methods.withName = function (name) {
+    this.name = name;
+    return this;
+}
+
+sessionSchema.methods.withDescription = function (description) {
+    this.description = description;
+    return this;
+}
+
+sessionSchema.methods.withImage = function (image) {
+    this.image = image;
+    return this;
+}
+
+sessionSchema.methods.withDateRange = function (startDate, endDate) {
+    this.date.start = startDate;
+    this.date.end = endDate;
+    return this;
+}
+
+sessionSchema.methods.withSourceCodeLink = function (sourceCodeLink) {
+    this.resources.sourceCodeLink = sourceCodeLink;
+    return this;
+}
+
+sessionSchema.methods.withSlidesLink = function (slidesLink) {
+    this.resources.slidesLink = slidesLink;
+    return this;
+}
+
+sessionSchema.methods.withVideoLink = function (videoLink) {
+    this.resources.videoLink = videoLink;
+    return this;
+}
+
+sessionSchema.methods.withSubmissionLink = function (submissionLink) {
+    this.project.submissionLink = submissionLink;
+    return this;
+}
+
+/**
+ * Creates and saves an initialized Session object
+ * @param {Session} initialized session
+ * @returns {Session} newly saved Session object
+ * @example
+ * Session.init()
+ *        .withName("name")
+ *        .withDescription("desc")
+ *        .withImage("imageURL")
+ *        .withDateRange(new Date(), new Date())
+ *        .build()
+ *        .then(session => console.log(session));
+ */
+sessionSchema.methods.build = function () {
+    return new Promise((resolve, reject) => {
+        this.save((error, newSession) => {
+            if (error) reject(error);
+            else resolve(newSession);
+        });
+    });
 };
 
-Session.statics.findById = function(id) {
-	return this.findOne({ id }).exec();
+/**
+ * Fetches all sessions
+ * @returns {Session[]} all saved Session objects
+ * @example
+ * Session.getAll()
+ *     .then(sessions => console.log(sessions))
+ *     .catch(error => console.error(error));
+ */
+sessionSchema.statics.getAll = function () {
+    return new Promise((resolve, reject) => {
+        this.find({}, (error, sessions) => {
+            if (error) reject(error);
+            else resolve(sessions);
+        });
+    });
 };
 
-Session.statics.findSessionForDate = function(date) {
-	return this.findOne({ 'date.start' : { $lt : new Date(date) }, 'date.end' : { $gt : new Date(date) } }).exec();
-};
-
-module.exports = Session;
+module.exports = sessionSchema;
