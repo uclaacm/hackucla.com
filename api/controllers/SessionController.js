7d2fe1794f6e8e309335642554c10452ff2652f4
shannonphu <shannonphu@sbcglobal.net>
2018-04-14 17:04:45 -0700

    implement session endpoints

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -5,11 +5,47 @@
  * @param {Mongoose.Model} MongoDB Session model
  */
 module.exports = function (Session) { // eslint-disable-line
+	function getAll(req, res, next) {
+		Session.getAll()
+			.then(sessions => res.json(sessions))
+			.catch(error => {
+				return next(error);
+			});
+	}
+
+	function create(req, res, next) {
+		Session.init()
+			.withName(req.body.name)
+			.withDescription(req.body.description)
+			.withImage(req.body.image)
+			.withDateRange(req.body.startDate, req.body.endDate)
+			.withSourceCodeLink(req.body.sourceCodeLink)
+			.withSlidesLink(req.body.slidesLink)
+			.withVideoLink(req.body.videoLink)
+			.withSubmissionLink(req.body.submissionLink)
+			.build()
+			.then(session => res.json(session))
+			.catch(error => {
+				return next(error);
+			});
+	}
+
+	function deleteSession(req, res, next) {
+		Session.delete(req.params.id)
+			.then(res.status(200).end())
+			.catch(error => {
+				return next(error);
+			});
+	}
+
 	function test(req, res) {
 		res.json('/session/test endpoint hit');
 	}
 
 	return {
+		getAll,
+		create,
+		deleteSession,
 		test
 	};
 };
--- api/routes/session.js
+++ api/routes/session.js
@@ -5,7 +5,10 @@ module.exports = function (sessionDb) {
 
 	const sessionController = require('../controllers/SessionController')(sessionDb);
 
+	router.get('/', sessionController.getAll);
 	router.get('/test', sessionController.test);
+	router.post('/new', sessionController.create);
+	router.delete('/:id', sessionController.deleteSession);
 
 	return router;
 };
