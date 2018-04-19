804a3751a37ecdb6ad37277a1184dde6affda77f
shannonphu <shannonphu@sbcglobal.net>
2018-04-18 22:07:42 -0700

    Migrate to axios http lib

--- client/src/utils/httpHelper.js
+++ client/src/utils/httpHelper.js
@@ -1,55 +1,24 @@
-const config = require('../config');
-const baseURL = `http://${config.server.host}:${config.server.port}`;
+import axios from 'axios';
 
-const headers = new Headers();
-
-headers.set('Content-Type', 'application/json');
-
-const reqConf = {
-	headers: headers,
-	credentials: 'include',
-};
-
-// Helper functions for the comon request types
-export function smartFetch(method, endpoint, body) {
-	return fetch(baseURL + endpoint, {
-		method: method,
-		body: body && JSON.stringify(body),
-		...reqConf
-	}).catch((err) => {
-		return Promise.reject(`${method} ${endpoint} with body ${body} failed: ${err}`);
-	});
+/**
+ * make a get request
+ * @param {string} URL
+ * @returns {Promise}
+ */
+export function get(url) {
+	return axios.get(url)
+		.then(response => response.data)
+		.catch((error) => Promise.reject(`GET ${url} failed: ${error}`));
 }
 
 /**
  * make a get request
- * @param {string} endpoint
+ * @param {string} URL
+ * @param {Object} request body
  * @returns {Promise}
  */
-export function get(endpoint) {
-	return smartFetch('GET', endpoint);
-}
-
-/**
- * make a post request
- * @param {string} endpoint
- * @param {Object} body
- * @returns {Promise}
- */
-export function post(endpoint, body) {
-	return smartFetch('POST', endpoint, body);
-}
-
-/**
- * make a put request
- * @param {string} endpoint
- * @param {Object} body
- * @returns {Promise}
- */
-export function put(endpoint, body) {
-	return smartFetch('PUT', endpoint, body);
-}
-
-export function del(endpoint) {
-	return smartFetch('DELETE', endpoint);
+export function post(url, requestBody) {
+	return axios.post(url, requestBody)
+		.then(response => response.data)
+		.catch((error) => Promise.reject(`POST ${url} failed: ${error}`));
 }
