7a6a4d44e70043926808fbcda1e42beb8fedb1b5
Kristie Lim <kristie.lim@gmail.com>
2018-05-07 22:11:03 -0700

    add rectangle

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -21,6 +21,7 @@ class Homepage extends Component {
 					<img className={styles.logo} src={blackLogo} />
 					<div className={styles.page2_textBox}>
 						<div className={styles.text_poppins + ' ' + styles.page2_title}>What is Hack?</div>
+						<svg className={styles.rect}><rect className={styles.rect}></rect></svg>
 						<div className={styles.text_chivo + ' ' + styles.page2_whoweare}>
 							{'We are a student-run organization that aims to empower UCLA students to influence their world through code.' 
 							+ ' We host events for coders of all skill levels. So whether you\'ve been to 10 hackathons, or you just learned'
