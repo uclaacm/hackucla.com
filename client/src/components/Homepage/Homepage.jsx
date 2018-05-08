5060df48aeac017ad69e008e3530d8f1eac7baeb
Kristie Lim <kristie.lim@gmail.com>
2018-05-07 21:45:31 -0700

    update fonts

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -11,17 +11,17 @@ class Homepage extends Component {
 				<div className={styles.page1}>
 					<img className={styles.img} src={splashBackground} />
 					<div className={styles.page1_textBox}>
-						<div className={styles.text + ' ' + styles.page1_tagline}>Move Fast<span className={styles.red}>.</span></div>
-						<div className={styles.text + ' ' + styles.page1_tagline}>Build Things<span className={styles.red}>.</span></div>
-						<div className={styles.text + ' ' + styles.page1_subtagline}>Start Hacking.</div>
+						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Move Fast<span className={styles.red}>.</span></div>
+						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Build Things<span className={styles.red}>.</span></div>
+						<div className={styles.text_poppins + ' ' + styles.page1_subtagline}>Start Hacking.</div>
 					</div>
 				</div>
 				<div className={styles.page2}>
 					<img className={styles.img} src={page2Background} />
 					<img className={styles.logo} src={blackLogo} />
 					<div className={styles.page2_textBox}>
-						<div className={styles.text + ' ' + styles.page2_title}>What is Hack?</div>
-						
+						<div className={styles.text_poppins + ' ' + styles.page2_title}>What is Hack?</div>
+						<div className={styles.text_chivo + ' ' + styles.page2_whoweare}>{'We are a student-run organization that aims to empower UCLA students to influence their world through code. We host events for coders of all skill levels. So whether you\'ve been to 10 hackathons, or you just learned "hello world," we\'re happy to have you.'} </div>
 					</div>
 				</div>
 			</div>
