a417af5962107f0137cc87acce80ff498f09a7f4
shannonphu <shannonphu@gmail.com>
2018-05-23 12:27:11 -0700

    cleanup css

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -9,7 +9,7 @@ class Homepage extends Component {
 		return (
 			<div>
 				<div className={styles.page1}>
-					<img className={styles.img} src={splashBackground} />
+					<img className={styles.splashImg} src={splashBackground} />
 					<div className={styles.page1_textBox}>
 						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Move Fast<span className={styles.red}>.</span></div>
 						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Build Things<span className={styles.red}>.</span></div>
@@ -17,16 +17,18 @@ class Homepage extends Component {
 					</div>
 				</div>
 				<div className={styles.page2}>
-					<img className={styles.img} src={page2Background} />
-					<img className={styles.logo} src={blackLogo} />
-					<div className={styles.page2_textBox}>
-						<div className={styles.text_poppins + ' ' + styles.page2_title}>What is Hack?</div>
-						<hr className={styles.rect}/>
-						<div className={styles.text_chivo + ' ' + styles.page2_whoweare}>
-							{`We are a student-run organization that aims to empower UCLA students to influence their world through code. 
+					<img className={styles.splashImg} src={page2Background} />
+					<div className={styles.page2Content}>
+						<div className={styles.page2_textBox}>
+							<div className={styles.text_poppins + ' ' + styles.page2_title}>What is Hack?</div>
+							<div className={styles.rectContainer}><hr className={styles.rect} /></div>
+							<div className={styles.text_chivo + ' ' + styles.page2_whoweare}>
+								{`We are a student-run organization that aims to empower UCLA students to influence their world through code. 
 							We host events for coders of all skill levels. So whether you've been to 10 hackathons, or you just learned
-							"hello world," we're happy to have you.`} 
+							"hello world," we're happy to have you.`}
+							</div>
 						</div>
+						<img className={styles.logo} src={blackLogo} />						
 					</div>
 				</div>
 			</div>
