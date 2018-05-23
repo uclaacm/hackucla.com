4286973bbeb6726cc900b530ae6a85015bdbfb8a
Kristie Lim <kristie.lim@gmail.com>
2018-05-23 12:33:54 -0700

    Added "What is Hack?" to homepage (#8)
    * add hack logo
    
    * make text responsive
    
    * update fonts
    
    * fix paragraph
    
    * add rectangle
    
    * changed font weight
    
    * moved images
    
    * changed backgrounds to svg
    
    * made requested changes
    
    * cleanup css
    
    * fix warnings about alt attr in img tag

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import splashBackground from '../../homepage_images/wrenches.png';
-import styles from './homepage.css';
+import splashBackground from './images/SplashBackground.svg';
+import page2Background from './images/Page2Background.svg';
+import blackLogo from './images/logo-hack-black.png';
+import styles from './Homepage.css';
 
 class Homepage extends Component {
 	render() {
 		return (
-			<div className={styles.page1}>
-				<img className={styles.img} src={splashBackground} />
-				<div className={styles.page1_textBox}>
-					<div className={styles.text + ' ' + styles.page1_tagline}>Move Fast<span className={styles.red}>.</span></div>
-					<div className={styles.text + ' ' + styles.page1_tagline}>Build Things<span className={styles.red}>.</span></div>
-					<div className={styles.text + ' ' + styles.page1_subtagline}>Start Hacking.</div>
+			<div>
+				<div className={styles.page1}>
+					<img className={styles.splashImg} src={splashBackground} alt="gears in background" />
+					<div className={styles.page1_textBox}>
+						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Move Fast<span className={styles.red}>.</span></div>
+						<div className={styles.text_poppins + ' ' + styles.page1_tagline}>Build Things<span className={styles.red}>.</span></div>
+						<div className={styles.text_poppins + ' ' + styles.page1_subtagline}>Start Hacking.</div>
+					</div>
+				</div>
+				<div className={styles.page2}>
+					<img className={styles.splashImg} src={page2Background} alt="red and purple blocks" />
+					<div className={styles.page2Content}>
+						<div className={styles.page2_textBox}>
+							<div className={styles.text_poppins + ' ' + styles.page2_title}>What is Hack?</div>
+							<div className={styles.rectContainer}><hr className={styles.rect} /></div>
+							<div className={styles.text_chivo + ' ' + styles.page2_whoweare}>
+								{`We are a student-run organization that aims to empower UCLA students to influence their world through code. 
+							We host events for coders of all skill levels. So whether you've been to 10 hackathons, or you just learned
+							"hello world," we're happy to have you.`}
+							</div>
+						</div>
+						<img className={styles.logo} src={blackLogo} alt="Hack logo of wrench in light bulb" />						
+					</div>
 				</div>
 			</div>
 		);
